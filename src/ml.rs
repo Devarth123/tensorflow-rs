7fc21e850dbfefeec4639079856296ebb0a4d477
Devarth123 <devarthsarayan485@gmail.com>
2022-06-06 14:04:56 +0530

    left some stupid errors

--- src/ml.rs
+++ src/ml.rs
@@ -1,5 +1,7 @@
-pub mod matrix;
-pub mod nn;
-pub mod read;
-pub mod activation;
-pub mod gernerate;
+// pub mod matrix;
+// pub mod nn;
+// pub mod read;
+// pub mod activation;
+// pub mod gernerate;
+pub mod f32;
+pub mod f64;
