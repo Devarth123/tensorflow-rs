a0a9d8a794c5550bebe3b7266528e77ac632cfe2
Devarth123 <devarthsarayan485@gmail.com>
2022-06-14 23:05:42 +0530

    adding stuff/fixing broken stuff

--- src/ml/f64/img.rs
+++ src/ml/f64/img.rs
@@ -1,8 +1,15 @@
 use crate::ml::f64::matrix;
 
 pub struct Img {
-    label: u8,
-    matrix: matrix::MatrixStruct,
+    pub label: u8,
+    pub matrix: matrix::MatrixStruct,
 }
 
-
+impl Img{
+    pub fn new() -> Img{
+        Img{
+            label: 0,
+            matrix: matrix::matrix_create(&28, &28)
+        }
+    }
+}
--- src/ml/f64/read.rs
+++ src/ml/f64/read.rs
@@ -1,21 +1,7 @@
 use crate::ml::f64::img;
-use crate::ml::f64::matrix;
-use std::fs;
+use std::fs::File;
+use std::io::prelude::Read;
 
-// pub fn breaker(not_formated_string: &str) -> Vec<String> {
-//     // print!("{not_formated_string}");
-//     let mut formated_string: Vec<String> = Vec::new();
-//     let mut push_str = String::new();
-//     for i in not_formated_string.chars() {
-//         if i == '\n' {
-//             formated_string.push(push_str.clone());
-//         } else {
-//             push_str.push(i);
-//         }
-//     }
-//     formated_string
-// }
-//
 pub fn count_new_lines(string: &str) -> u16 {
     let mut new_lines = 0;
     for i in string.chars() {
@@ -25,63 +11,34 @@ pub fn count_new_lines(string: &str) -> u16 {
     }
     new_lines
 }
-// pub fn matrix_fit(unformated_data: &Vec<f64>) -> matrix::MatrixStruct{
-//
-// }
-// pub fn matrix_fit(unformated_data: &Vec<f64>) -> matrix::MatrixStruct{
-//     let mut matrix = matrix::matrix_create(&28, &28);
-//     for i in 0..28{
-//        let mut multiplier = 0;
-//        for j in 0..28{
-//            matrix.matrix[i][j] = unformated_data[j+multiplier];
-//        }
-//        multiplier += 28;
-//     }
-//     matrix
-// }
-// pub fn read_csv(path_of_the_file: &str, how_many_lines_do_want: &u8) -> Vec<Vec<f64>> {
-//     let data = breaker(&fs::read_to_string(&path_of_the_file).unwrap());
-//     let mut counter = 0;
-//     let mut str_to_f32: Vec<Vec<f64>> = Vec::new();
-//     for i in &data {
-//         let mut push_vec: Vec<f64> = vec![0.0; 28];
-//         for j in i.chars() {
-//             match j {
-//                 j if j != ',' => push_vec.push(
-//                     j.to_string()
-//                         .parse::<f64>()
-//                         .expect("the method at line 64 is not working as expected"),
-//                 ),
-//                 '\n' => counter += 1,
-//                 _ => (),
-//             }
-//             if counter == *how_many_lines_do_want {
-//                 break;
-//             }
-//         }
-//         str_to_f32.push(push_vec);
-//     }
-//     str_to_f32
-// }
-pub fn  line(line_no: &u16, lines: &std::str::Lines) -> String{
-    let mut str = "";
-    for i in 0..*line_no{
-        if i == line_no -1 {
-            str = lines.next().unwrap();
-        }else {
-            lines.next();
+
+
+pub fn format_data<const LEN: usize>(string: &[u8], lines: &usize) -> Vec<img::Img> {
+    let mut imgs: Vec<img::Img> = Vec::with_capacity(*lines);
+    let mut a: [f64; LEN] = [0.0; LEN];
+    let stupid = 0;
+    for i in 0..string.len() {
+        if string[i] != b',' {
+            if string[i] != b'\n' {
+                a[i] = string[i].to_string().parse::<f64>().unwrap();
+            } else {
+                let mut img = img::Img::new();
+                for j in 0..28{
+                        img.matrix.matrix[j] = a[stupid..i].to_vec(); 
+                }
+                imgs.push(img);
+                let stupid = stupid + 28;
+            }
         }
-    } 
-    str.to_string()
-}
-pub fn read_csv(path_of_the_file: &str, lines: &u8) -> matrix::MatrixStruct {
-    let imgs: Vec<Vec<img::Img>> = Vec::with_capacity(*lines as usize);
-    static MAXCHAR: u16 = 10000;
-    let lines: String = (fs::read_to_string(path_of_the_file).unwrap()).lines();
-    let mut i: u16  = 0;
-    while i < *lines {
-        let j = 0;
-        x
-        i += 1;
     }
+    imgs
+}
+pub fn read_csv(path_of_the_file: &str, lines: &usize) -> Vec<img::Img> {
+    const MAXCHAR: usize = 10000; //this numbr cuz each linne has basically 10000 bytes
+    let mut string: [u8; MAXCHAR] = [0; MAXCHAR];
+    {
+        let mut file = File::open(path_of_the_file).unwrap();
+        File::read_exact(&mut file, &mut string).unwrap();
+    }
+    format_data::<MAXCHAR>(&string, lines)
 }
