6f15b54643547b5bc18dc63a35c8756771c3ba6f
Devarth123 <devarthsarayan485@gmail.com>
2022-06-14 15:05:57 +0530

    bad commit

--- src/ml/f32/nn.rs
+++ src/ml/f32/nn.rs
@@ -1,48 +1,42 @@
 //will be optimizing the code very soon.
-use crate::ml::f32::activation;
 use crate::ml::f32::matrix;
+use crate::ml::f32::activation;
 
-pub struct NeuralNetwork<'a> {
+pub struct NeuralNetwork<'a>{
     parameters: &'a [u16], //the first index will be the input layer (parameters[0]) , the last index will be the output layer (parameters[parameters.len()-1]) and lastly the rest of the vaues will be the hidden_layer (parameters[1..parameters.len()-2])
     learning_rate: f32,
-    hidden_weights: matrix::MatrixStruct,
-    output_weights: matrix::MatrixStruct,
+    hidden_weights: matrix::MatrixStruct, 
+    output_weights: matrix::MatrixStruct
 }
 
-pub fn neural_network<'a>(parameters_: &'a [u16], learning_rate_: &'a f32) -> NeuralNetwork<'a> {
-    NeuralNetwork {
-        parameters: parameters_,
-        learning_rate: *learning_rate_,
-        hidden_weights: matrix::matrix_create(&parameters_[1], &parameters_[0]),
-        output_weights: matrix::matrix_create(&parameters_[parameters_.len() - 1], &parameters_[1]),
+pub fn neural_network<'a>(parameters_: &'a [u16], learning_rate_: &'a f32) -> NeuralNetwork<'a>{
+  NeuralNetwork{
+      parameters:  parameters_,
+      learning_rate: *learning_rate_,
+      hidden_weights: matrix::matrix_create(&parameters_[1], &parameters_[0]),
+      output_weights: matrix::matrix_create(&parameters_[parameters_.len()-1], &parameters_[1])
+  } 
+}
+pub fn neural_network_train64(nn: &mut NeuralNetwork, input: &matrix::MatrixStruct, output: &matrix::MatrixStruct){
+    {
+        //doing teh basic things to find the errors
+        let hidden_inputs = matrix::dot(&nn.hidden_weights, input);
+        let hidden_ouputs = activation::apply(&(0), &hidden_inputs); 
+        let final_inputs = matrix::dot(&nn.output_weights, &hidden_ouputs);
+        let final_outputs = activation::apply(&(0), &final_inputs);
+        //finding the error
+        let output_errors =  matrix::subtract(&output, &final_outputs);   // and we are done
+        let hidden_errors  = matrix::dot(&(matrix::transpose(&nn.output_weights)), &output_errors);
+        // applying the sigmoid_prime method on the final outputa and mutiplying it with the output
+        // error  
+        // let sigmoid_prime_matrix = activation::sigmoid_prime64(&final_outputs); 
+        let multiplied_matrix = matrix::multiply(&output_errors , &(activation::sigmoid_prime(&final_outputs))); 
+        let dot_matrix = matrix::dot(&multiplied_matrix, &matrix::transpose(&hidden_ouputs));
+        let scaled_matrix = matrix::scale(&nn.learning_rate, &dot_matrix);
+        nn.output_weights = matrix::add(&nn.output_weights, &scaled_matrix);  
+        let multiplied_matrix = matrix::multiply(&hidden_errors, &(activation::sigmoid_prime(&hidden_ouputs)));
+        let dot_matrix = matrix::dot(&multiplied_matrix, &matrix::transpose(&input));
+        let scaled_matrix = matrix::scale(&nn.learning_rate, &dot_matrix);
+        nn.hidden_weights = matrix::add(&nn.hidden_weights, &scaled_matrix);
     }
 }
-pub fn neural_network_train(
-    nn: &mut NeuralNetwork,
-    input: &matrix::MatrixStruct,
-    output: &matrix::MatrixStruct,
-) {
-    //doing teh basic things to find the errors
-    let hidden_inputs = matrix::dot(&nn.hidden_weights, input);
-    let hidden_ouputs = activation::apply(&(0), &hidden_inputs);
-    let final_inputs = matrix::dot(&nn.output_weights, &hidden_ouputs);
-    let final_outputs = activation::apply(&(0), &final_inputs);
-    //finding the error
-    let output_errors = matrix::subtract(output, &final_outputs); // and we are done
-    let hidden_errors = matrix::dot(&(matrix::transpose(&nn.output_weights)), &output_errors);
-    // applying the sigmoid_prime method on the final outputa and mutiplying it with the output
-    // error
-    // let sigmoid_prime_matrix = activation::sigmoid_prime64(&final_outputs);
-    let multiplied_matrix =
-        matrix::multiply(&output_errors, &(activation::sigmoid_prime(&final_outputs)));
-    let dot_matrix = matrix::dot(&multiplied_matrix, &matrix::transpose(&hidden_ouputs));
-    let scaled_matrix = matrix::scale(&nn.learning_rate, &dot_matrix);
-    nn.output_weights = matrix::add(&nn.output_weights, &scaled_matrix);
-    let multiplied_matrix =
-        matrix::multiply(&hidden_errors, &(activation::sigmoid_prime(&hidden_ouputs)));
-    let dot_matrix = matrix::dot(&multiplied_matrix, &matrix::transpose(input));
-    let scaled_matrix = matrix::scale(&nn.learning_rate, &dot_matrix);
-    nn.hidden_weights = matrix::add(&nn.hidden_weights, &scaled_matrix);
-}
-
-
--- src/ml/f64/gernerate.rs
+++ src/ml/f64/gernerate.rs
@@ -1,7 +1,13 @@
 extern crate rand;
+<<<<<<< HEAD
 use rand::Rng;
-use std::collections::HashMap;
+=======
 
+>>>>>>> parent of 7d001fc (formated)
+use std::collections::HashMap;
+use rand::Rng;
+
+<<<<<<< HEAD
 pub fn storing_labels<'a>(formated_data: &'a Vec<f32>, labels: &mut HashMap<u16, &'a [f32]>) {
     labels.insert(formated_data[0] as u16, &formated_data[1..]);
 }
@@ -17,5 +23,18 @@ pub fn gernerate_imgs<'a>(
     match labels.get(&random_img) {
         Some(vec) => *vec,
         None => panic!("the random gernerate_imgs() method is not working at line 49"),
+=======
+
+pub fn storing_labels<'a>(formated_data: &'a Vec<f32>, labels: &mut HashMap<u16, &'a[f32]>){
+         labels.insert(formated_data[0] as u16, &formated_data[1..]); 
+ }
+pub fn gernerate_imgs<'a>(labels: HashMap<u16, &'a Vec<Vec<f32>>>, range: &u32) -> &'a Vec<Vec<f32>>{
+       let random_img: u16 = rand::thread_rng().gen_range(0, *range + 1) as u16;
+       // let random_img: u16 = crate::Rng.get_range(0, range);
+       match labels.get(&random_img){
+              Some(vec) => *vec,
+              None => panic!("the random gernerate_imgs() method is not working at line 49") 
+         }
+>>>>>>> parent of 7d001fc (formated)
     }
-}
+
