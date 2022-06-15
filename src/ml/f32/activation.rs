f6639bffdf9744eb27f1259427329c07f74d0a41
Devarth123 <devarthsarayan485@gmail.com>
2022-06-15 13:16:03 +0530

    updating f32 mod

--- src/ml/f32/activation.rs
+++ src/ml/f32/activation.rs
@@ -1,32 +1,31 @@
-use crate::ml::f32::matrix;
+use crate::ml::f32::matrix::MatrixStruct;
 
-pub fn exp(double: &f32) -> f32{
+pub fn exp(double: &f32) -> f32 {
     double * std::f32::consts::E //2.71828
 }
 
-pub fn sigmoid(input: &f32) -> f32{
-    1.0 / (1.0 + exp(&(-1.0*input)))
+pub fn sigmoid(input: &f32) -> f32 {
+    1.0 / (1.0 + exp(&(-1.0 * input)))
 }
 
-
-pub fn sigmoid_prime(m: &matrix::MatrixStruct) -> matrix::MatrixStruct{
-    let mut matrix = matrix::sigmoid_matrix(&m.rows, &m.columns);
+pub fn sigmoid_prime(m: &MatrixStruct) -> MatrixStruct {
+    let mut matrix = MatrixStruct::matrix_create(&m.rows, &m.columns);
     {
-        let subtracted_matrix = matrix::subtract(&matrix, m);
-        matrix = matrix::subtract(m, &subtracted_matrix);
+        let subtracted_matrix = MatrixStruct::subtract(&matrix, m);
+        matrix = MatrixStruct::subtract(m, &subtracted_matrix);
     }
     matrix
 }
 
-// 0 means sigmoid and 1 relu (not added yet) 
-pub fn apply(function: &u8, m: &matrix::MatrixStruct) -> matrix::MatrixStruct{
-    let mut matrix = matrix::matrix_create(&m.rows, &m.columns);
-   if *function == 0{
-      for i in 0..m.rows as usize{
-          for j in 0..m.columns as usize{
-              matrix.matrix[i][j] = sigmoid(&(m.matrix[i][j]));
-          }
-      }  
-   }  
-   matrix
+// 0 means sigmoid and 1 relu (not added yet)
+pub fn apply(function: &u8, m: &MatrixStruct) -> MatrixStruct {
+    let mut matrix = MatrixStruct::matrix_create(&m.rows, &m.columns);
+    if *function == 0 {
+        for i in 0..m.rows {
+            for j in 0..m.columns {
+                matrix.matrix[i][j] = sigmoid(&(m.matrix[i][j]));
+            }
+        }
+    }
+    matrix
 }
